94238116b0b182b97a3e1aa476f7d952129f9135
olganow <olganow@yandex.ru>
2023-04-26 11:46:26 +0300

    feat: UserService, User Controller

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.InvalidEmailException;
+import ru.yandex.practicum.catsgram.exception.UserAlreadyExistException;
+import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.PostService;
+import ru.yandex.practicum.catsgram.service.UserService;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+ // внедрение зависимостей через конструктор.
+ @Autowired
+ // Это значит, что здесь произойдёт внедрение зависимостей — создание UserService и его передача в UserContoller произойдёт автоматически.
+ public UserController(UserService userService) {
+     this.userService = userService;
+ }
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+        return userService.updateUser(user);
+    }
+}

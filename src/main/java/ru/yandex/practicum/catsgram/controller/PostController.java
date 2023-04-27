afc7d66c9fcfaf913f92040155e360513c7e4f3c
olganow <olganow@yandex.ru>
2023-04-27 12:15:03 +0300

    feat: PostController

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -21,11 +21,26 @@ public class PostController {
         this.postService = postService;
     }
 
+    //Дополните метод findAll класса-контроллера PostController параметрами запроса: sort — строка со значениями asc
+    // (от англ. ascending, «восходящий» — по возрастанию) или desc (от англ. descending, «нисходящий» — по убыванию),
+    // page и size — целочисленные, size — больше нуля.
     @GetMapping("/posts")
-    public List<Post> findAll() {
-        return postService.findAll();
-    }
+    public List<Post> findAll(
+            @RequestParam(value = "page", defaultValue = "0", required = false) Integer page,
+            @RequestParam(value = "size", defaultValue = "10", required = false) Integer size,
+            @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
 
+
+        if(!(sort.equals("asc") || sort.equals("desc"))){
+            throw new IllegalArgumentException();
+        }
+        if(page < 0 || size <= 0){
+            throw new IllegalArgumentException();
+        }
+
+        Integer from = page * size;
+        return postService.findAll(size, from, sort);
+    }
     @PostMapping(value = "/post")
     public Post create(@RequestBody Post post) {
         return postService.create(post);
--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -5,14 +5,23 @@ import ru.yandex.practicum.catsgram.model.Post;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
     private final List<Post> posts = new ArrayList<>();
-
-    public List<Post> findAll() {
-        return posts;
-    }
+//Доработайте метод findAll класса-сервиса PostService. Из общего списка публикаций должен возвращаться список первых
+// или последних постов, начиная с заданного порядкового номера. Посты должны быть отсортированы по дате создания.
+// Для этого добавьте в метод аргументы size, sort и from.
+public List<Post> findAll(Integer size, Integer from, String sort) {
+    return posts.stream().sorted((p0, p1) -> {
+        int comp = p0.getCreationDate().compareTo(p1.getCreationDate()); //прямой порядок сортировки
+        if(sort.equals("desc")){
+            comp = -1 * comp; //обратный порядок сортировки
+        }
+        return comp;
+    }).skip(from).limit(size).collect(Collectors.toList());
+}
 
     public Post create(Post post) {
         posts.add(post);

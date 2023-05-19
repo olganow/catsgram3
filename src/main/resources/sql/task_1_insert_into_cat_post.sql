8eec4373ca477260fe3fcbdd1a16fdbf7d25cfa6
olganow <olganow@yandex.ru>
2023-05-19 15:28:21 +0300

    feat: sql

---
+++ src/main/resources/sql/task_1_insert_into_cat_post.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date) VALUES ('tom', 'мой кот', 'file:///storage/catsgram/tom/1/image.png', '2023-03-28 19:22:29.000000');
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date) VALUES ('tom', 'кот )', 'file:///storage/catsgram/tom/2/image.png', '2023-03-12 15:32:54.000000');
+INSERT INTO public.cat_post (author_id, description, photo_url, creation_date) VALUES ('grumpy', 'это я', 'file:///storage/catsgram/grumpy/1/image.png', '2023-02-28 15:24:03.000000');
